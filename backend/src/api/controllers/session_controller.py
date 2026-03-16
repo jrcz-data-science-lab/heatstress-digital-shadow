b3e373ae38562eeb1dda826163fdf83237bff6ca
ItsDimitrie <laue0002@HZ.nl>
2026-03-16 14:31:37 +0100

    PR feedback

--- backend/src/api/controllers/session_controller.py
+++ backend/src/api/controllers/session_controller.py
@@ -2,8 +2,11 @@ from abc import ABC
 from fastapi import Cookie, Response
 from typing import Optional
 import os
+import re
 import zipfile
 import tempfile
+
+_UUID_RE = re.compile(r'^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$')
 
 from src.api.session import create_session
 
@@ -25,20 +28,27 @@ def _copy_and_fix_qgz(src: str, dst: str) -> None:
         with zipfile.ZipFile(src, 'r') as z:
             z.extractall(tmpdir)
 
-        for fname in os.listdir(tmpdir):
-            if fname.endswith('.qgs'):
-                fpath = os.path.join(tmpdir, fname)
-                with open(fpath, 'r', encoding='utf-8') as f:
-                    content = f.read()
-                content = content.replace('<datasource>./', '<datasource>../../')
-                with open(fpath, 'w', encoding='utf-8') as f:
-                    f.write(content)
-
-        with zipfile.ZipFile(dst, 'w', zipfile.ZIP_DEFLATED) as z:
-            for root, dirs, files in os.walk(tmpdir):
-                for fname in files:
+        for root, dirs, files in os.walk(tmpdir):
+            for fname in files:
+                if fname.endswith('.qgs'):
                     fpath = os.path.join(root, fname)
-                    z.write(fpath, os.path.relpath(fpath, tmpdir))
+                    with open(fpath, 'r', encoding='utf-8') as f:
+                        content = f.read()
+                    content = content.replace('<datasource>./', '<datasource>../../')
+                    with open(fpath, 'w', encoding='utf-8') as f:
+                        f.write(content)
+
+        dst_tmp = dst + '.tmp'
+        try:
+            with zipfile.ZipFile(dst_tmp, 'w', zipfile.ZIP_DEFLATED) as z:
+                for root, dirs, files in os.walk(tmpdir):
+                    for fname in files:
+                        fpath = os.path.join(root, fname)
+                        z.write(fpath, os.path.relpath(fpath, tmpdir))
+            os.replace(dst_tmp, dst)
+        finally:
+            if os.path.exists(dst_tmp):
+                os.unlink(dst_tmp)
 
 
 class SessionController(ABC):
@@ -58,7 +68,7 @@ class SessionController(ABC):
 
         # 1. Get or create session id
         created = False
-        if session_id:
+        if session_id and _UUID_RE.match(session_id):
             sid = session_id
         else:
             sid = create_session()
